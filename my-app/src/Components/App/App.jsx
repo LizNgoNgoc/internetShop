398c738e8befd16e9fd6ec01525d32058207bf6b
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-02 16:39:12 +0300

    add Home.jsx component

--- my-app/src/Components/App/App.jsx
+++ my-app/src/Components/App/App.jsx
@@ -1,10 +1,12 @@
 import Header from "../Header/Header";
+import Home from "../Home/Home";
 
 
 function App() {
   return (
     <div className='mr-0 box-border'>
        <Header/>
+       <Home/>
     </div>
   );
 }
---
+++ my-app/src/Components/Home/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return <section className="bg-[url('./images/home_page//Mask Group.jpeg')] w-full">
+
+    </section>
+}
