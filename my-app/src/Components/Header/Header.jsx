b34264160ccf1ff7de90fbc3c813149bd8297e4e
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-02 14:12:21 +0300

    add fonts styles

--- my-app/src/Components/Header/Header.jsx
+++ my-app/src/Components/Header/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
             </Link>
         </div>
         <div>
-            <nav className="flex w-auto gap-16 mx-40">
+            <nav className="flex w-auto gap-16 mx-40 font-medium">
                 <Link>Home</Link>
                 <Link>Shop</Link>
                 <Link>About</Link>
