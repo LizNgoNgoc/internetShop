02b0c557177d661ab244b7b984205f27e26093b9
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-02 20:15:25 +0300

    add css and pictures for the Range.jsx

--- my-app/src/Components/App/App.jsx
+++ my-app/src/Components/App/App.jsx
@@ -1,5 +1,6 @@
 import Header from "../Header/Header";
 import Home from "../Home/Home";
+import Range from "../Range.jsx/Range";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='mr-0 box-border'>
        <Header/>
        <Home/>
+       <Range/>
     </div>
   );
 }
--- my-app/src/Components/Range.jsx/Range.jsx
+++ my-app/src/Components/Range.jsx/Range.jsx
@@ -1,5 +1,23 @@
 export default function Range() {
-    return <section>
-        
+    return <section className="w-full mt-14 px-32">
+        <div className="w-auto text-center mb-16">
+            <h3 className="font-bold text-3xl text-zinc-900">Browse The Range</h3>
+            <p className="text-zinc-500 text-xl font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="flex gap-5">
+            <div className="flex flex-col gap-8">
+                <img src="./images/range/Dining.png" alt="" />
+                <h4 className="text-center text-zinc-900 font-semibold text-2xl">Dining</h4>
+            </div>
+            <div className="flex flex-col gap-8">
+                <img src="./images/range/living.png" alt="" />
+                <h4 className="text-center text-zinc-900 font-semibold text-2xl">Living</h4>
+            </div>
+            <div className="flex flex-col gap-8">
+                <img src="./images/range/bedroom.png" alt="" />
+                <h4 className="text-center text-zinc-900 font-semibold text-2xl">Bedroom</h4>
+            </div>
+        </div>
+
     </section>
 }
