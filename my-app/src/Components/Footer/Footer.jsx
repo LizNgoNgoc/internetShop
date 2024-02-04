41774cc934864913289c811cbaf067b2118ebb01
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-04 11:22:28 +0300

    add footer component

---
+++ my-app/src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer(){
+    return <section className="w-full">
+        <div className="flex gap-14">
+            <div>
+                <h3 className="font-bold text-3xl text-zinc-900">Funiro.</h3>
+                <p className="text-zinc-100 text-xl font-normal">400 University Drive Suite 200 Coral Gables,
+FL 33134 USA</p>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </section>
+}
--- my-app/src/Components/Furniture/Furniture.jsx
+++ my-app/src/Components/Furniture/Furniture.jsx
@@ -1,5 +1,12 @@
 export default function Furniture() {
-    return <section>
-        
+    return <section className="w-full mt-14 px-32">
+        <div className="w-auto text-center mb-16">
+            <p className="text-zinc-500 text-xl font-normal">Share your setup with</p>
+            <h3 className="font-bold text-3xl text-zinc-900">#FuniroFurniture</h3>
+        </div>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
     </section>
 }
