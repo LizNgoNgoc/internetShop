d732e06b1719a5d26afdf9b135f9dcd8ead042c1
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-05 16:16:16 +0300

    add css shop block

--- my-app/src/Components/HeaderShop/HeaderShop.jsx
+++ my-app/src/Components/HeaderShop/HeaderShop.jsx
@@ -1,8 +1,189 @@
 export default function HeaderShop() {
-    return <section className="w-full mb-14">
+    return <section className="w-full mb-14 p-24">
         <img src="./images/shopHeader/banner.png" alt="shop"/>
         <div className="absolute top-56 right-2/4">
             <h3 className="m-auto text-zinc-900 text-5xl">Shop</h3>
+        </div>
+        <div className="bg-orange-100 h-28">
+
+        </div>
+        <div className="flex flex-col gap-8 mt-10">
+        <div className="flex gap-8">
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Syltherpng.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Syltherine</h4>
+                        <p className="font-medium text-base text-zinc-500">Stylish cafe chair</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 2.500.000</span>
+                            <strike className="font-medium text-base text-zinc-500">Rp 3.500.000</strike>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Leviosa.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Leviosa</h4>
+                        <p className="font-medium text-base text-zinc-500">Stylish cafe chair</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 2.500.000</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Lolito.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Lolito</h4>
+                        <p className="font-medium text-base text-zinc-500">Luxury big sofa</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 7.000.000</span>
+                            <strike className="font-medium text-base text-zinc-500">Rp 14.000.000</strike>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Respiro.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Respira</h4>
+                        <p className="font-medium text-base text-zinc-500">Outdoor bar table and stool</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 500.000</span>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <div className="flex gap-8">
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Syltherpng.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Syltherine</h4>
+                        <p className="font-medium text-base text-zinc-500">Stylish cafe chair</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 2.500.000</span>
+                            <strike className="font-medium text-base text-zinc-500">Rp 3.500.000</strike>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Leviosa.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Leviosa</h4>
+                        <p className="font-medium text-base text-zinc-500">Stylish cafe chair</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 2.500.000</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Lolito.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Lolito</h4>
+                        <p className="font-medium text-base text-zinc-500">Luxury big sofa</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 7.000.000</span>
+                            <strike className="font-medium text-base text-zinc-500">Rp 14.000.000</strike>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Respiro.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Respira</h4>
+                        <p className="font-medium text-base text-zinc-500">Outdoor bar table and stool</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 500.000</span>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <div className="flex gap-8">
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Syltherpng.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Syltherine</h4>
+                        <p className="font-medium text-base text-zinc-500">Stylish cafe chair</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 2.500.000</span>
+                            <strike className="font-medium text-base text-zinc-500">Rp 3.500.000</strike>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Leviosa.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Leviosa</h4>
+                        <p className="font-medium text-base text-zinc-500">Stylish cafe chair</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 2.500.000</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Lolito.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Lolito</h4>
+                        <p className="font-medium text-base text-zinc-500">Luxury big sofa</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 7.000.000</span>
+                            <strike className="font-medium text-base text-zinc-500">Rp 14.000.000</strike>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Respiro.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Respira</h4>
+                        <p className="font-medium text-base text-zinc-500">Outdoor bar table and stool</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 500.000</span>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <div className="flex gap-8">
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Syltherpng.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Syltherine</h4>
+                        <p className="font-medium text-base text-zinc-500">Stylish cafe chair</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 2.500.000</span>
+                            <strike className="font-medium text-base text-zinc-500">Rp 3.500.000</strike>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Leviosa.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Leviosa</h4>
+                        <p className="font-medium text-base text-zinc-500">Stylish cafe chair</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 2.500.000</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Lolito.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Lolito</h4>
+                        <p className="font-medium text-base text-zinc-500">Luxury big sofa</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 7.000.000</span>
+                            <strike className="font-medium text-base text-zinc-500">Rp 14.000.000</strike>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <img src="./images/range/Respiro.png" alt="img" />
+                    <div>
+                        <h4 className=" text-zinc-900 font-semibold text-2xl">Respira</h4>
+                        <p className="font-medium text-base text-zinc-500">Outdoor bar table and stool</p>
+                        <div className="flex gap-4">
+                            <span className=" text-zinc-900 font-semibold text-2xl">Rp 500.000</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 }
