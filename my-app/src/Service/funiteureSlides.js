c3a170f5b87edbaa11e07cd7c7c6c1bc716d1441
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-11 15:26:51 +0300

    add changes div

--- my-app/src/Service/funiteureSlides.js
+++ my-app/src/Service/funiteureSlides.js
@@ -9,3 +9,13 @@ export const arrCards = [
     '/images/furniture/f8.png',
     '/images/furniture/f9.png',
 ]
+
+
+arrCards.forEach(src => {
+    const img = document.createElement('img')
+    img.src = src
+
+    const div = document.createElement('div')
+    div.className = 'card'
+    div.append(img)
+})
