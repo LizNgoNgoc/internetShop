4dfd68664d88b27e9844a32925265cde682ab359
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-16 15:23:30 +0300

    add #colors

--- my-app/src/Components/HeaderShop/HeaderShop.jsx
+++ my-app/src/Components/HeaderShop/HeaderShop.jsx
@@ -8,20 +8,20 @@ export default function HeaderShop() {
     return <section className="w-full mb-14 p-24 font-Poppins">
         <img className="w-full" src="./images/shopHeader/banner.png" alt="shop"/>
         <div className="absolute top-56 right-2/4">
-            <h3 className="m-auto text-zinc-900 text-5xl">Shop</h3>
+            <h3 className="m-auto text-[#3A3A3A] text-5xl">Shop</h3>
         </div>
-        <div className="bg-orange-100 h-28"></div>
+        <div className="bg-[#F9F1E7] h-28"></div>
         <div className="flex flex-col gap-8 mt-10">
         <div className="flex gap-8">
             {arrProd.map((item) => {
                 return <div className="flex flex-col gap-4" key={item.id}>
                 <img src={item.img} alt="img" />
                     <div>
-                        <h4 className=" text-zinc-900 font-semibold text-2xl">{item.name}</h4>
-                        <p className="font-medium text-base text-zinc-500">{item.cardDescription}</p>
+                        <h4 className=" text-[#3A3A3A] font-semibold text-2xl">{item.name}</h4>
+                        <p className="font-medium text-base text-[#898989]">{item.cardDescription}</p>
                         <div className="flex gap-4">
-                            <span className=" text-zinc-900 font-semibold text-2xl">Rp.{item.price}</span>
-                            <strike className="font-medium text-base text-zinc-500">Rp.{item.oldPrice}</strike>
+                            <span className=" text-[#3A3A3A] font-semibold text-2xl">Rp.{item.price}</span>
+                            <strike className="font-medium text-base text-[#898989]">Rp.{item.oldPrice}</strike>
                         </div>
                     </div>
                 </div>
@@ -32,11 +32,11 @@ export default function HeaderShop() {
                 return <div className="flex flex-col gap-4" key={item.id}>
                 <img src={item.img} alt="img" />
                     <div>
-                        <h4 className=" text-zinc-900 font-semibold text-2xl">{item.name}</h4>
-                        <p className="font-medium text-base text-zinc-500">{item.cardDescription}</p>
+                        <h4 className=" text-[#3A3A3A] font-semibold text-2xl">{item.name}</h4>
+                        <p className="font-medium text-base text-[#898989]">{item.cardDescription}</p>
                         <div className="flex gap-4">
-                            <span className=" text-zinc-900 font-semibold text-2xl">Rp.{item.price}</span>
-                            <strike className="font-medium text-base text-zinc-500">Rp.{item.oldPrice}</strike>
+                            <span className=" text-[#3A3A3A] font-semibold text-2xl">Rp.{item.price}</span>
+                            <strike className="font-medium text-base text-[#898989]">Rp.{item.oldPrice}</strike>
                         </div>
                     </div>
                 </div>
@@ -47,11 +47,11 @@ export default function HeaderShop() {
                 return <div className="flex flex-col gap-4" key={item.id}>
                 <img src={item.img} alt="img" />
                     <div>
-                        <h4 className=" text-zinc-900 font-semibold text-2xl">{item.name}</h4>
-                        <p className="font-medium text-base text-zinc-500">{item.cardDescription}</p>
+                        <h4 className=" text-[#3A3A3A] font-semibold text-2xl">{item.name}</h4>
+                        <p className="font-medium text-base text-[#898989]">{item.cardDescription}</p>
                         <div className="flex gap-4">
-                            <span className=" text-zinc-900 font-semibold text-2xl">Rp.{item.price}</span>
-                            <strike className="font-medium text-base text-zinc-500">Rp.{item.oldPrice}</strike>
+                            <span className=" text-[#3A3A3A] font-semibold text-2xl">Rp.{item.price}</span>
+                            <strike className="font-medium text-base text-[#898989]">Rp.{item.oldPrice}</strike>
                         </div>
                     </div>
                 </div>
@@ -62,11 +62,11 @@ export default function HeaderShop() {
                 return <div className="flex flex-col gap-4" key={item.id}>
                 <img src={item.img} alt="img" />
                     <div>
-                        <h4 className=" text-zinc-900 font-semibold text-2xl">{item.name}</h4>
-                        <p className="font-medium text-base text-zinc-500">{item.cardDescription}</p>
+                        <h4 className=" text-[#3A3A3A] font-semibold text-2xl">{item.name}</h4>
+                        <p className="font-medium text-base text-[#898989]">{item.cardDescription}</p>
                         <div className="flex gap-4">
-                            <span className=" text-zinc-900 font-semibold text-2xl">Rp.{item.price}</span>
-                            <strike className="font-medium text-base text-zinc-500">Rp.{item.oldPrice}</strike>
+                            <span className=" text-[#3A3A3A] font-semibold text-2xl">Rp.{item.price}</span>
+                            <strike className="font-medium text-base text-[#898989]">Rp.{item.oldPrice}</strike>
                         </div>
                     </div>
                 </div>
