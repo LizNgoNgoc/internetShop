785c916a093b210e4eda428e8b03d19ac620836a
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-16 16:02:47 +0300

    add styles for the shop page

--- my-app/src/Components/HeaderShop/HeaderShop.jsx
+++ my-app/src/Components/HeaderShop/HeaderShop.jsx
@@ -7,10 +7,21 @@ export default function HeaderShop() {
 
     return <section className="w-full mb-14 p-24 font-Poppins">
         <img className="w-full" src="./images/shopHeader/banner.png" alt="shop"/>
-        <div className="absolute top-56 right-2/4">
-            <h3 className="m-auto text-[#3A3A3A] text-5xl">Shop</h3>
+        <div className="absolute top-72 right-1/4">
+            <h3 className="text-[#000000] text-5xl">Shop</h3>
+                <div className="flex gap-2 items-center">
+                    <span className="text-[#000000] font-medium text-base">Home </span>
+                    <img src="./images/shopHeader/point_page.png" className="w-[8px] h-[14px]" alt="img" />
+                    <span className="text-[#000000] font-light text-base"> Shop</span>
+                </div>
         </div>
-        <div className="bg-[#F9F1E7] h-28"></div>
+        <div className="bg-[#F9F1E7] h-28 flex gap-28 px-44 items-center alighn-center">
+            <img src="./images/shopHeader/filter.png" className="w-[25px] h-[25px]" alt="img" />
+            <p className="font-normal text-xl">Filter</p>
+            <img src="./images/shopHeader/cards_hum.png" className="w-[25px] h-[25px]" alt="img" />
+            <img src="./images/shopHeader/hum.png" className="w-[25px] h-[25px]" alt="img" />
+            <p className="font-normal text-xl">Showing 1–16 of 32 results</p>
+        </div>
         <div className="flex flex-col gap-8 mt-10">
         <div className="flex gap-8">
             {arrProd.map((item) => {
