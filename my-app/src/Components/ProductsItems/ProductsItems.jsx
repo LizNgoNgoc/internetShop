fd700ef80fceb60ee06a566cc806b049233f3179
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-18 14:19:25 +0300

    add css pages

--- my-app/src/Components/ProductsItems/ProductsItems.jsx
+++ my-app/src/Components/ProductsItems/ProductsItems.jsx
@@ -56,7 +56,7 @@ export default function ProductsItems() {
                     {arrProd.map((item) => {
                         return <div className="flex flex-col gap-4" key={item.id}>
                         <img src={item.img} alt="img" />
-                            <div>
+                            <div className="bg-[#F4F5F7]">
                                 <h4 className=" text-[#3A3A3A] font-semibold text-2xl">{item.name}</h4>
                                 <p className="font-medium text-base text-[#898989]">{item.cardDescription}</p>
                                 <div className="flex gap-4">
@@ -67,11 +67,11 @@ export default function ProductsItems() {
                         </div>
                     })}
                 </div>
-                <div className="flex w-auto gap-9 mb-11">
-                    <Link><img src="./images/shopHeader/click_square.png" className="relative" alt="img" /><p className="absolute">1</p></Link>
-                    <Link><img src="./images/shopHeader/square.png" className="relative" alt="img" /><p className="absolute">2</p></Link>
-                    <Link><img src="./images/shopHeader/square.png" className="relative" alt="img" /><p className="absolute">3</p></Link>
-                    <Link><img src="./images/shopHeader/next_before.png" className="relative" alt="img" /><p className="absolute">Next</p></Link>
+                <div className="flex w-auto gap-9 mb-11 justify-center">
+                    <Link><div className="bg-[#B88E2F] font-normal text-white text-xl w-[60px] h-[60px] rounded-xl p-5">1</div></Link>
+                    <Link><div className="bg-[#F9F1E7] font-normal text-black text-xl w-[60px] h-[60px] rounded-xl p-5">2</div></Link>
+                    <Link><div className="bg-[#F9F1E7] font-normal text-black text-xl w-[60px] h-[60px] rounded-xl p-5">3</div></Link>
+                    <Link><div className="bg-[#F9F1E7] font-light text-black text-xl w-[98px] h-[60px] rounded-xl p-5">Next</div></Link>
                 </div>
             </div>
     </srction>
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
     extend: {
       backgroundImage: {
         'homePage-bg': "url('/public/images/home_page/Mask_Group.jpeg')",
+      },
+      overlayImage: {
+        'overlayImg': "url('/public/images/range/Overlay.png')"
       }
     },
   },
