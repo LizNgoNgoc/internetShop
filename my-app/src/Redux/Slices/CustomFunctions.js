4c2307a168b35428fa4c4fa7cca1baced9ac678b
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-23 20:35:35 +0300

    create custom Slice

---
+++ my-app/src/Redux/Slices/CustomFunctions.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+}
+
+export const funcSlice = ({
+    name: 'click',
+    initialState,
+    reducers: {
+
+    }
+})
+
+export const {} = funcSlice.actions
+export default funcSlice.reducer
