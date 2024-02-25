8a65bce8c7d633055407c198df72fcd105c14764
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-25 22:01:01 +0300

    add css changes and add funcSlice to the store

--- my-app/src/Custom/ModalBasket/ModalBasket.jsx
+++ my-app/src/Custom/ModalBasket/ModalBasket.jsx
@@ -6,26 +6,26 @@ export default function ModalBasket() {
     const dispatch = useDispatch()
 
     return<section className="w-[417px] h-[746px] absolute top-7 right-2.5 p-[20px] bg-white font-Poppins"onClick={() => dispatch(hideModal)}>
-        <div className="flex gap-8">
+        <div className="flex gap-40 mb-6">
             <h3 className="font-semibold text-2xl">Shopping Cart</h3>
             <img src="./images/modal/closeBasket.png" className="w-7" alt="" />
         </div>
         <div className="w-full border-[1px] border-[#D9D9D9] divide-solid"></div>
-        <div className="w-full flex flex-col gap-3 py-5 overflow-y-scroll h-[550px] mt-5">
+        <div className="w-full flex flex-col gap-3 py-5 overflow-y-scroll h-[465px] mt-5">
             <div className="w-full flex items-center gap-8">
                 <img src="./images/range/Leviosa.png" className="w-[108px] h-[105px]" alt="img" />
                 <div className="">
                     <h3>Asgaard sofa</h3>
                     <div className="flex justify-center items-center gap-[15px]">
-                        <p>1 x </p><p className="text-[#B88E2F] text-[12px] leading-0 text font-medium">Rs. 250,000.00</p>
+                        <p>1 x </p><p className="text-[12px] leading-0 text font-medium text-[#B88E2F]">Rs. 250,000.00</p>
                     </div>
                 </div>
                 <img src="./images/modal/close.png"  className='w-[28px] h-[28px]' alt="img" />
             </div>
         </div>
-        <div className="flex gap-24">
+        <div className="flex gap-24 mb-5 pl-[27px] pr-[40px] py-[28px]">
             <h2 className="font-normal text-base text-black">Subtotal</h2>
-            <p className="font-semibold text-base">Rs. 520,000.00</p>
+            <p className="font-semibold text-base text-[#B88E2F]">Rs. 520,000.00</p>
         </div>
         <div className="w-full border-[#D9D9D9] border-[1px] divide-solid"></div>
         <div className="flex gap-3.5 mt-[26px]">
--- my-app/src/Redux/store.js
+++ my-app/src/Redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { funcSlice } from "./Slices/CustomFunctions";
 
 
 export const store = configureStore ({
     reducer : {
-       
+        funcSlice,
     }
 })
