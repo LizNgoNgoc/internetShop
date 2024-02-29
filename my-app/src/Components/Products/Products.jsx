a5d3a3c2df0d7245d567b84174a0d5f964ad1769
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-02-29 13:29:52 +0300

    add settingdProducts,js

--- my-app/src/Components/Products/Products.jsx
+++ my-app/src/Components/Products/Products.jsx
@@ -1,96 +1,23 @@
+import { arrProducts } from "./SettingsProducts"
+
 export default function Productds() {
     return <section className="w-full mt-14 p-24 font-Poppins">
         <h3 className="font-bold text-3xl text-[#333333] text-center mb-8">Our Products</h3>
-        <div className="flex flex-col gap-8">
-            <div className="flex gap-8">
-                <div className="flex flex-col gap-4">
-                    <img src="./images/range/Syltherpng.png" alt="img" />
-                    <div>
-                        <h4 className=" text-[#333333] font-semibold text-2xl">Syltherine</h4>
-                        <p className="font-medium text-base text-[#898989]">Stylish cafe chair</p>
-                        <div className="flex gap-4">
-                            <span className=" text-[#333333] font-semibold text-2xl">Rp 2.500.000</span>
-                            <strike className="font-medium text-base text-[#898989]">Rp 3.500.000</strike>
-                        </div>
+            <div className="flex gap-8 flex-wrap">
+                {arrProducts.map((item, index) => {
+                    return <div className="flex flex-col gap-4 mb-8" key={index}>
+                            <img src={item.img}alt="img" />
+                            <div>
+                                <h4 className=" text-[#333333] font-semibold text-2xl">{item.name}</h4>
+                                <p className="font-medium text-base text-[#898989]">{item.description}</p>
+                                <div className="flex gap-4">
+                                    <span className=" text-[#333333] font-semibold text-2xl">{item.price}</span>
+                                    <strike className="font-medium text-base text-[#898989]">{item.oldPrice}</strike>
+                                </div>
+                            </div>
                     </div>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <img src="./images/range/Leviosa.png" alt="img" />
-                    <div>
-                        <h4 className=" text-[#333333] font-semibold text-2xl">Leviosa</h4>
-                        <p className="font-medium text-base text-[#898989]">Stylish cafe chair</p>
-                        <div className="flex gap-4">
-                            <span className=" text-[#333333] font-semibold text-2xl">Rp 2.500.000</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <img src="./images/range/Lolito.png" alt="img" />
-                    <div>
-                        <h4 className=" text-[#333333] font-semibold text-2xl">Lolito</h4>
-                        <p className="font-medium text-base text-[#898989]">Luxury big sofa</p>
-                        <div className="flex gap-4">
-                            <span className=" text-[#333333]font-semibold text-2xl">Rp 7.000.000</span>
-                            <strike className="font-medium text-base text-[#898989]">Rp 14.000.000</strike>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <img src="./images/range/Respiro.png" alt="img" />
-                    <div>
-                        <h4 className=" text-[#333333] font-semibold text-2xl">Respira</h4>
-                        <p className="font-medium text-base text-[#898989]">Outdoor bar table and stool</p>
-                        <div className="flex gap-4">
-                            <span className=" text-[#333333] font-semibold text-2xl">Rp 500.000</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="flex gap-8">
-                <div className="flex flex-col gap-4">
-                    <img src="./images/range/Grifo.png" alt="img" />
-                    <div>
-                        <h4 className=" text-[#333333] font-semibold text-2xl">Grifo</h4>
-                        <p className="font-medium text-base text-[#898989] ">Night lamp</p>
-                        <div className="flex gap-4">
-                            <span className=" text-[#333333] font-semibold text-2xl">Rp 1.500.000</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <img src="./images/range/Muggo.png" alt="img" />
-                    <div>
-                        <h4 className=" text-[#333333] font-semibold text-2xl">Muggo</h4>
-                        <p className="font-medium text-base text-[#898989]">Small mug</p>
-                        <div className="flex gap-4">
-                            <span className=" text-[#333333] font-semibold text-2xl">Rp 150.000</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <img src="./images/range/Pingkypng.png" alt="img" />
-                    <div>
-                        <h4 className=" text-[#333333] font-semibold text-2xl">Pingky</h4>
-                        <p className="font-medium text-base text-[#898989]">Cute bed set</p>
-                        <div className="flex gap-4">
-                            <span className=" text-[#333333] font-semibold text-2xl">Rp 7.000.000</span>
-                            <strike className="font-medium text-base text-[#898989]">Rp 14.000.000</strike>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <img src="./images/range/Potty.png" alt="img" />
-                    <div>
-                        <h4 className=" text-[#333333] font-semibold text-2xl">Potty</h4>
-                        <p className="font-medium text-base text-[#898989]">Minimalist flower pot</p>
-                        <div className="flex gap-4">
-                            <span className=" text-[#333333] font-semibold text-2xl">Rp 500.000</span>
-                        </div>
-                    </div>
-                </div>
+                })}
             </div>
             <button className="text-yellow-600 border-yellow-600 font-bold w-60 h-12 border-2 block my-0 mx-auto">Show More</button>
-        </div>
-        
     </section>
 }
---
+++ my-app/src/Components/Products/SettingsProducts.js
@@ -0,0 +1,10 @@
+export const arrProducts = [
+    {img: './images/range/Syltherpng.png', name: 'Syltherine', description: 'Stylish cafe chair', price: 'Rp 2.500.000', oldPrice: 'Rp 3.500.000'},
+    {img: './images/range/Leviosa.png', name: 'Leviosa', description: 'Stylish cafe chair', price: 'Rp 2.500.000', oldPrice: ''},
+    {img: './images/range/Lolito.png', name: 'Lolito', description: 'Luxury big sofa', price: 'Rp 7.000.000', oldPrice: 'Rp 14.000.000'},
+    {img: './images/range/Respiro.png', name: 'Respira', description: 'Outdoor bar table and stool', price: 'Rp 500.000', oldPrice: ''},
+    {img: './images/range/Grifo.png', name: 'Grifo', description: 'Night lamp', price: 'Rp 1.500.000', oldPrice: ''},
+    {img: './images/range/Muggo.png', name: 'Muggo', description: 'Small mug', price: 'Rp 150.000', oldPrice: ''},
+    {img: './images/range/Pingkypng.png', name: 'Pingky', description: 'Cute bed set', price: 'Rp 7.000.000', oldPrice: 'Rp 14.000.000'},
+    {img: './images/range/Potty.png', name: 'Potty', description: 'Minimalist flower pot', price: 'Rp 500.000', oldPrice: ''},
+]
