943ae6403d463b1b886cd9fef13acc245ca4d329
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-03-04 13:33:40 +0300

    update product css

--- my-app/src/Pages/Product.jsx
+++ my-app/src/Pages/Product.jsx
@@ -16,13 +16,25 @@ export default function Product () {
     
 
 
-    return <section className={"font-Poppins my-10 mx-[auto] w-[80%]"}>
-        <h1>{productCard.name}</h1>
-        <img src={productCard.img} alt="img"/>
-        <p>{productCard.cardDescription}</p>
-        <p>{productCard.price}</p>
-        <p>{productCard.oldPrice}</p>
-        <p>{productCard.description}</p>
-        <button className="bg-[#B88E2F] w-[202px] h-[48px]" onClick={() => dispatch(addToCart(productCard))}>Add to cart</button>
+    return <section className="font-Poppins my-10 mx-[auto] w-[80%] flex">
+        <div className="mr-20">
+            <img src={productCard.img} alt="img"/>  
+        </div>
+        <div>
+            <h1>{productCard.cardDescription}</h1>
+            <div className="flex gap-4">
+                <p>Rp.{productCard.price}</p>
+                <strike>Rp.{productCard.oldPrice}</strike>
+            </div>
+            <p>{productCard.description}</p>
+            <p>Size</p>
+            <p>Color</p>
+            <div className="w-[605px] bg-[#D9D9D9]"></div>
+        <div>
+            <button className="border-[1px] border-black rounded-3xl text-[12px] text-black py-[6px] px-[30px]">1</button>
+            <button className="border-[1px] border-black rounded-3xl text-[12px] text-black py-[6px] px-[30px]" onClick={() => dispatch(addToCart(productCard))}>Add to cart</button>
+            <button className="border-[1px] border-black rounded-3xl text-[12px] text-black py-[6px] px-[30px]">+ Compare</button>
+        </div>  
+        </div>
     </section>
 }
