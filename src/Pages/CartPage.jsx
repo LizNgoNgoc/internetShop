e0399f6fdfceec20c96d719fe8a89e20951ac3e7
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-03-17 22:08:40 +0300

    add cart css

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -1,8 +1,8 @@
 export default function CartPage() {
     return <section className="m-auto w-[1440px] mb-14 p-24 font-Poppins relative">
-    <img className="w-full" src="./images/shopHeader/banner.png" alt="shop"/>
-    <div className="absolute top-72 right-1/4">
-        <h3 className="text-[#000000] text-5xl">Shop</h3>
+    <img className="w-full" src="./images/shopHeader/banner.png" alt="cart"/>
+    <div className="z-10 absolute">
+        <h3 className="text-[#000000] text-5xl">Cart</h3>
             <div className="flex gap-2 items-center">
                 <span className="text-[#000000] font-medium text-base">Home </span>
                 <img src="./images/shopHeader/point_page.png" className="w-[8px] h-[14px]" alt="img" />
