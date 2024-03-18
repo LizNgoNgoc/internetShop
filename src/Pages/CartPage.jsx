a52a2eb1fdc93f7514a3d700e9c7ee7c6b83b80f
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-03-18 15:55:48 +0300

    update css Cart

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -4,12 +4,12 @@ import Garantees from "../Components/Garantees/Garantees"
 export default function CartPage() {
     return <section className="m-auto w-[1440px] mb-14 p-24 font-Poppins relative">
             <img className="w-full" src="./images/shopHeader/banner.png" alt="cart"/>
-            <div className="absolute top-52 right-[45%]">
+            <div className="absolute top-[13rem] right-0 left-0 w-[135px] m-auto">
                 <h3 className="text-[#000000] text-5xl">Cart</h3>
                     <div className="flex gap-2 items-center">
                         <span className="text-[#000000] font-medium text-base">Home </span>
                         <img src="./images/shopHeader/point_page.png" className="w-[8px] h-[14px]" alt="img" />
-                        <span className="text-[#000000] font-light text-base"> Shop</span>
+                        <span className="text-[#000000] font-light text-base"> Cart</span>
                     </div>
             </div>
             <div className="w-full pt-[72px] pb-[63px] flex justify-between ">
