ac37a38c6f31fbb857da0c34dc8f21a2df12a6bf
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-03-22 12:26:41 +0300

    change the top div-135

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -11,7 +11,7 @@ export default function CartPage() {
 
     return <section className="m-auto w-[1440px] mb-14 p-24 font-Poppins relative">
             <img className="w-full" src="./images/shopHeader/banner.png" alt="cart"/>
-            <div className="absolute top-[13rem] right-0 left-0 w-[105px] m-auto">
+            <div className="absolute top-[10rem] right-0 left-0 w-[105px] m-auto">
                 <img src="./images/modal/logo.png" alt="logo" className="m-auto "/>
                 <h3 className="text-[#000000] text-5xl">Cart</h3>
                     <div className="flex gap-2 items-center">
