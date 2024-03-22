4b29943e24658891913a0d2c89aa5dfe34bc4e3d
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-03-22 12:25:41 +0300

    change width for div-135

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -11,8 +11,8 @@ export default function CartPage() {
 
     return <section className="m-auto w-[1440px] mb-14 p-24 font-Poppins relative">
             <img className="w-full" src="./images/shopHeader/banner.png" alt="cart"/>
-            <div className="absolute top-[13rem] right-0 left-0 w-[135px] m-auto">
-                <img src="./images/modal/logo.png" alt="logo" />
+            <div className="absolute top-[13rem] right-0 left-0 w-[105px] m-auto">
+                <img src="./images/modal/logo.png" alt="logo" className="m-auto "/>
                 <h3 className="text-[#000000] text-5xl">Cart</h3>
                     <div className="flex gap-2 items-center">
                         <span className="text-[#000000] font-medium text-base">Home </span>
