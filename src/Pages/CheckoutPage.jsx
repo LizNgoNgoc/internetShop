772514fc173deac77886d9754564e526074302e5
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-03-25 13:14:22 +0300

    add header justify

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -5,7 +5,7 @@ export default function Checkout() {
             <div className="absolute top-[10rem] right-0 left-0 w-[235px] m-auto">
                 <img src="./images/modal/logo.png" alt="logo" className="m-auto "/>
                 <h3 className="text-[#000000] text-5xl">Checkout</h3>
-                    <div className="flex gap-2 items-center">
+                    <div className="flex gap-2 items-center justify-center">
                         <span className="text-[#000000] font-medium text-base">Home </span>
                         <img src="./images/shopHeader/point_page.png" className="w-[8px] h-[14px]" alt="img" />
                         <span className="text-[#000000] font-light text-base"> Checkout</span>
